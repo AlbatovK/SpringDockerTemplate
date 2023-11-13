37c319fb62919a1ec6fbd7631f2db5ad8d48740c
AlbatovK <albatovkonstantin@yandex.ru>
2023-11-13 21:24:54 +0300

    :art: CI image upload workflow + static analysis fix № 3

--- src/main/kotlin/com/albatros/springtemplate/OpenApiConfiguration.kt
+++ src/main/kotlin/com/albatros/springtemplate/OpenApiConfiguration.kt
@@ -2,6 +2,7 @@ package com.albatros.springtemplate
 
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
 import org.springframework.context.annotation.Bean
@@ -17,8 +18,12 @@ class OpenApiConfiguration {
                 title = "SpringDockerTemplate"
                 description = "PostgreSQL + Docker + Github workflow CI/CD Spring boot backend template"
                 version = "v0.0.1"
-                license = License().name("Apache 2.0")
-
+                license = License().name("MIT License").url("https://choosealicense.com/licenses/mit/")
+                contact = Contact().apply {
+                    email = "albatovkonstantin@yandex.ru"
+                    name = "AlbatovK"
+                    url = "https://github.com/AlbatovK"
+                }
             }
         )
         .externalDocs(
